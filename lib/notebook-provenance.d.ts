3d414f3415689b4fcf4791972002edda05e66caf
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2020-09-15 07:50:27 +0200

    Revert "revert to 25th"
    This reverts commit 2117ad880433adcba7050f5e3731edc40c396dd4.

--- lib/notebook-provenance.d.ts
+++ lib/notebook-provenance.d.ts
@@ -1,6 +1,4 @@
-import { JupyterLab } from '@jupyterlab/application';
 import { INotebookModel, Notebook } from '@jupyterlab/notebook';
-import { ISessionContext } from '@jupyterlab/apputils';
 import { Provenance } from '@visdesignlab/trrack';
 import { NotebookProvenanceTracker } from './provenance-tracker';
 import { PartialJSONValue } from '@lumino/coreutils';
@@ -19,24 +17,23 @@ export interface ApplicationState {
 }
 export interface ApplicationExtra {
     changedCellId: number;
-    relations?: number[];
+    cellPositions?: number[];
 }
-export declare type EventTypes = "Change active cell" | "executeCell" | "addCell" | "removeCell" | "moveCell" | "setCell" | "changeCellValue";
+export declare type EventTypes = "Active cell" | "executeCell" | "addCell" | "removeCell" | "moveCell" | "setCell" | "changeCellValue";
 export declare const EventTypes: string[];
 /**
  * Model for a provenance graph.
  */
 export declare class NotebookProvenance {
-    private app;
     readonly notebook: Notebook;
-    private sessionContext;
     private context;
+    private provenanceView;
     private _actionFunctions;
     private _nbtracker;
     private _prov;
     pauseTracking: boolean;
     pauseObserverExecution: boolean;
-    constructor(app: JupyterLab, notebook: Notebook, sessionContext: ISessionContext, context: DocumentRegistry.IContext<INotebookModel>);
+    constructor(notebook: Notebook, context: DocumentRegistry.IContext<INotebookModel>, provenanceView: any);
     private init;
     protected saveProvenanceGraph(): void;
     get nbtracker(): NotebookProvenanceTracker;
